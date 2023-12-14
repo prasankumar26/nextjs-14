efa5cc9c28a99277da3329b918fadf5ba71d7959
Prasan Kumar <90718794+prasankumar26@users.noreply.github.com>
2023-12-14 07:52:09 +0530

    Not Found Page | 404 Page

---
+++ src/app/products/[productId]/reviews/[reviewId]/not-found.tsx
@@ -0,0 +1,5 @@
+export default function NotFound() {
+    return <div>
+        <h4>Reviews Not Greater than 1000 Page Not Found </h4>
+    </div>
+}
--- src/app/products/[productId]/reviews/[reviewId]/page.tsx
+++ src/app/products/[productId]/reviews/[reviewId]/page.tsx
@@ -1,6 +1,14 @@
+import { notFound } from "next/navigation"
+
 export default function ProductDetails({params}:{params: {productId:string, reviewId:string}}) {
 
-return <h1 className="text-black">
+    if(parseInt(params.reviewId) > 1000){
+        notFound()
+    }
+
+return (
+    <h1 className="text-black">
     ProductDetails {params.productId} from {params.reviewId} Review
     </h1>
+)
 }
